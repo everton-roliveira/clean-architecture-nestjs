199a793083a14fa2d08c1df4fa1a9ddfb818e476
Everton Roberto de Oliveira <everton_roliveira@outlook.com>
2021-11-29 17:10:15 -0300

    feat: import EnvironmentConfigModule in typeorm

--- src/infra/config/typeorm/typeorm.module.ts
+++ src/infra/config/typeorm/typeorm.module.ts
@@ -1,29 +1,27 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { EnvironmentConfigModule } from '../environment-config/environment-config.module';
 import { EnvironmentConfigService } from '../environment-config/environment-config.service';
 
 export const getTypeOrmModuleOptions = (
   config: EnvironmentConfigService,
-): TypeOrmModuleOptions =>
-  ({
-    type: 'mysql',
-    host: config.getDatabaseHost(),
-    port: config.getDatabasePort(),
-    username: config.getDatabaseUser(),
-    password: config.getDatabasePassword(),
-    database: config.getDatabaseName(),
-    entities: [__dirname + './../../**/*.entity{.ts,.js}'],
-    synchronize: false,
-    schema: process.env.DATABASE_SCHEMA,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  } as TypeOrmModuleOptions);
-
+): TypeOrmModuleOptions => ({
+  type: 'mysql',
+  host: config.getDatabaseHost(),
+  port: config.getDatabasePort(),
+  username: config.getDatabaseUser(),
+  password: config.getDatabasePassword(),
+  database: config.getDatabaseName(),
+  entities: [__dirname + './../../**/*.entity{.ts,.js}'],
+  synchronize: false,
+  cli: {
+    migrationsDir: './src/infrastructure/database/migrations',
+  },
+});
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
-      imports: [EnvironmentConfigService],
+      imports: [EnvironmentConfigModule],
       inject: [EnvironmentConfigService],
       useFactory: getTypeOrmModuleOptions,
     }),
