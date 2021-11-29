22bdb3d42136e40b03cd61cbe5b5e6166d60eaae
Everton Roberto de Oliveira <everton_roliveira@outlook.com>
2021-11-29 18:09:39 -0300

    feat: execute in usecase make converte add user

--- src/usecases/user/add-user.usecase.ts
+++ src/usecases/user/add-user.usecase.ts
@@ -1,6 +1,6 @@
 import { IUseCase } from '@domain/base/use-case.interface';
 import { ILogger } from '@domain/logger/logger.interface';
-import { UserModel } from '@domain/model/user.model';
+import { UserModel, UserProps } from '@domain/model/user.model';
 import { IUserRepository } from '@domain/repositories/user-repository.interface';
 
 export class AddUserUseCase implements IUseCase<UserModel> {
@@ -9,7 +9,15 @@ export class AddUserUseCase implements IUseCase<UserModel> {
     private readonly repository: IUserRepository,
   ) {}
 
-  async execute(user: UserModel): Promise<UserModel> {
+  async execute(props: UserProps): Promise<UserModel> {
+    const userProps: UserProps = {
+      name: props.name,
+      lastName: props.lastName,
+      email: props.email,
+      password: props.password,
+      active: null,
+    };
+    const user: UserModel = UserModel.create(userProps);
     const result = await this.repository.insert(user);
     this.logger.log(AddUserUseCase.name, 'new user entered');
     return result;
