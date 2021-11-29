47603c0562cf6d98b687c55f3ed4171d16d77f52
Everton Roberto de Oliveira <everton_roliveira@outlook.com>
2021-11-29 17:11:03 -0300

    feat: use typeOrm in repository.module

--- src/infra/repositories/repositories.module.ts
+++ src/infra/repositories/repositories.module.ts
@@ -1,4 +1,12 @@
+import { TypeOrmConfigModule } from '@infra/config/typeorm/typeorm.module';
+import { UserEntity } from '@infra/entities/user.entity';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRepository } from './user.repository';
 
-@Module({})
+@Module({
+  imports: [TypeOrmConfigModule, TypeOrmModule.forFeature([UserEntity])],
+  providers: [UserRepository],
+  exports: [UserRepository],
+})
 export class RepositoriesModule {}
