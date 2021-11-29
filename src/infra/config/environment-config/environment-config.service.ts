fecd912c93b21a103ad4d2580ee71f3122c76264
Everton Roberto de Oliveira <everton_roliveira@outlook.com>
2021-11-29 15:45:25 -0300

    feat: config environment service to database

---
+++ src/infra/config/environment-config/environment-config.service.ts
@@ -0,0 +1,36 @@
+import { IDatabaseConfig } from '@domain/config/database-config.interface';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class EnvironmentConfigService implements IDatabaseConfig {
+  constructor(private configService: ConfigService) {}
+
+  getDatabaseHost(): string {
+    return this.configService.get<string>('DATABASE_HOST');
+  }
+
+  getDatabasePort(): number {
+    return this.configService.get<number>('DATABASE_PORT');
+  }
+
+  getDatabaseUser(): string {
+    return this.configService.get<string>('DATABASE_USER');
+  }
+
+  getDatabasePassword(): string {
+    return this.configService.get<string>('DATABASE_PASSWORD');
+  }
+
+  getDatabaseName(): string {
+    return this.configService.get<string>('DATABASE_NAME');
+  }
+
+  getDatabaseSchema(): string {
+    return this.configService.get<string>('DATABASE_SCHEMA');
+  }
+
+  getDatabaseSync(): boolean {
+    return this.configService.get<boolean>('DATABASE_SYNCHRONIZE');
+  }
+}
