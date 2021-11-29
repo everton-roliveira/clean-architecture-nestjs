e89b183c2a50f55ae31c0c5fe23292b58ffa5237
Everton Roberto de Oliveira <everton_roliveira@outlook.com>
2021-11-29 16:35:08 -0300

    feat: use pipes Validation to intercept request client, use log console and response client

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { AllExceptionFilter } from '@infra/common/filter/exception.filter';
+import { LoggingInterceptor } from '@infra/common/interceptors/logger.interceptor';
+import { ResponseInterceptor } from '@infra/common/interceptors/response.interceptor';
 import { LoggerService } from '@infra/logger/logger.service';
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -8,6 +11,13 @@ async function bootstrap() {
 
   // Filter
   app.useGlobalFilters(new AllExceptionFilter(new LoggerService()));
+
+  // pipes
+  app.useGlobalPipes(new ValidationPipe());
+
+  // interceptors
+  app.useGlobalInterceptors(new LoggingInterceptor(new LoggerService()));
+  app.useGlobalInterceptors(new ResponseInterceptor());
   await app.listen(3000);
 }
 bootstrap();
