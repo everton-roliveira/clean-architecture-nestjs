3570705d98561fc859f8c401f49eeae20fd45524
Everton Roberto de Oliveira <everton_roliveira@outlook.com>
2021-11-29 16:25:15 -0300

    feat: use exception filter with custom logger previously created

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import { AllExceptionFilter } from '@infra/common/filter/exception.filter';
+import { LoggerService } from '@infra/logger/logger.service';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Filter
+  app.useGlobalFilters(new AllExceptionFilter(new LoggerService()));
   await app.listen(3000);
 }
 bootstrap();
