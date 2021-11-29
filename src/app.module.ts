caae4eadf7ddde60b1a860c021daf9187475d3ac
Everton Roberto de Oliveira <everton_roliveira@outlook.com>
2021-11-29 18:08:16 -0300

    feat: use controller in the app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
+import { ControllersModule } from '@infra/controllers/controllers.module';
 import { ExceptionsModule } from '@infra/exceptions/exceptions.module';
 import { LoggerModule } from '@infra/logger/logger.module';
 import { UsecasesProxyModule } from '@infra/usecases-proxy/usecases-proxy.module';
 import { Module } from '@nestjs/common';
 @Module({
-  imports: [LoggerModule, ExceptionsModule, UsecasesProxyModule],
+  imports: [
+    LoggerModule,
+    ExceptionsModule,
+    UsecasesProxyModule,
+    ControllersModule,
+  ],
   controllers: [],
   providers: [],
 })
